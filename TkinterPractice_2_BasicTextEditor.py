4d57a8f832836536e62ed76e3072a895a7c29185
Arifur Rahman <arifur.rahman.cs04@gmail.com>
2025-03-27 23:22:51 +0600

    Started working on a basic text editor

---
+++ TkinterPractice_2_BasicTextEditor.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Basic Text Editior")
+
+
+window.columnconfigure(1, weight = 1, minsize = 800)
+window.rowconfigure(0, weight = 1, minsize = 800)
+
+text_box = tk.Text(master=window)
+text_box.grid(column = 1,row = 0, sticky = "nsew")
+
+btn_cont = tk.Frame(master = window, relief= tk.RAISED, bd = 2)
+btn_cont.grid(column=0, row=0, sticky="ns")
+
+open_btn = tk.Button(btn_cont, text = "Open")
+open_btn.grid(row = 0, column = 0, padx=5,pady=5,sticky="ew")
+
+save_btn = tk.Button(btn_cont, text = "Save as")
+save_btn.grid(row = 1, column = 0, padx=5,sticky="ew")
+              
+
+window.mainloop()
